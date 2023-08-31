ca7cbbf994510c2edb9ba7c35ff31d5331676727
Yarunoo <143714104+Yarunoo@users.noreply.github.com>
2023-08-31 16:41:54 +0200

    Update
    not done yet

--- index.php
+++ index.php
@@ -4,53 +4,74 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
-    <title>Yaruno Forms</title>
+    <title>Yaruno Forum</title>
+
+    <script>
+function toggleMode() {
+    var body = document.body;
+    var button = document.querySelector(".toggle");
+
+    if (body.classList.contains("light-mode")) {
+        body.classList.remove("light-mode");
+        body.classList.add("dark-mode");
+        button.textContent = "🌑";
+    } else {
+        body.classList.remove("dark-mode");
+        body.classList.add("light-mode");
+        button.textContent = "☀️";
+    }
+}
+</script>
+
 </head>
 <body>
-    <div class="container">
-        <h1>Yaruno Forms</h1>
-        <form action="postmessage.php" method="post">
 
-            <input type="text" name="name" maxlength="20" placeholder="Vul je naam in..." required>
-            <br>
-            <input type="text" name="text" maxlength="500" placeholder="Vul een nieuwe bericht in..." required>
+        <button class="toggle" onclick="toggleMode()">🌑</button>
 
-            <button type="submit">Plaats Bericht</button>
-        </form>
-        
-        <ul>
-            <?php
-            $messagesfile = "messages.txt";
+        <div class="chat">
+            <h1>Post a message</h1>
+            <hr class="seperator">
+            <form action="postmessage.php" method="post">
+                <input type="text" name="name" maxlength="20" placeholder="Enter your name..." required>
+                <br>
+                <textarea name="text" cols="56" placeholder="Enter your message..." rows="10" class="textbox"></textarea>
+                <br>
+                <button type="submit">Post Message</button>
+            </form>
+        </div>
+
+        <div class="container">
+            <h1>Yaruno Forum</h1>
+            <hr>
             
-            if(file_exists($messagesfile)) {
-                $messages = fopen($messagesfile, "r");
+            <ul>
+                <?php
+                $messagesfile = "messages.txt";
+                
+                if (file_exists($messagesfile)) {
+                    $messages = fopen($messagesfile, "r");
 
-                while(!feof($messages)) { 
-                    
-                    $line = fgets($messages);
-                    $jsondata = json_decode($line, true);
-                    
-                    if($jsondata != null) {
-
-                  
-                        if($jsondata["name"] && $jsondata["text"] && $jsondata["date"]) {
-                            $name = htmlspecialchars($jsondata["name"]);
-                            $text = htmlspecialchars($jsondata["text"]);
-                            $date = htmlspecialchars($jsondata["date"])
-                        ?> 
-
-                        <h3> <?php echo("$name - $date"); ?> </h3>   
-                        <li> <?php echo($text); ?> </li>
-                        <?php
+                    while (!feof($messages)) { 
+                        $line = fgets($messages);
+                        $jsondata = json_decode($line, true);
+                        
+                        if ($jsondata != null) {
+                            if ($jsondata["name"] && $jsondata["text"] && $jsondata["date"]) {
+                                $name = htmlspecialchars($jsondata["name"]);
+                                $text = htmlspecialchars($jsondata["text"]);
+                                $date = htmlspecialchars($jsondata["date"]);
+                                ?> 
+                                <h3><?php echo("$name - $date"); ?></h3>   
+                                <li><?php echo($text); ?></li>
+                                <?php
+                            }
                         }
                     }
+                    fclose($messages);
                 }
-                fclose($messages);
-            }
-            ?>     
-        </ul>
-    </div>
+                ?>     
+            </ul>
+        </div>
+
 </body>
 </html>
-
-   
